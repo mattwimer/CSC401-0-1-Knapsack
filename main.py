64f0de9b1c5d96bb27ee06c746fd6cbe5af3172c
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-12-14 21:41:44 -0800

    Just following the pseudocode basically line-for-line.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def knapsack(items, weights, values, capacity):
-    return None
+    n = len(items)
+    dp = [[0 for j in range(capacity+1)] for i in range(n+1)]
+
+    for i in range(n+1):
+        for w in range(capacity+1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif weights[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w], values[i-1] + dp[i-1][w-weights[i-1]])
+            else:
+                
